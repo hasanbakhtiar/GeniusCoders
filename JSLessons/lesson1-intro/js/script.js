95d338e3aa29ef8d83564df6bacdc8856bc54299
HAsan Bakhtiar <hasan.doctype@gmail.com>
2022-05-17 17:10:43 +0400

    js lesson enter

---
+++ JSLessons/lesson1-intro/js/script.js
@@ -0,0 +1,23 @@
+/**
+ * more line
+ * comment
+ */
+// one line
+// document.write('new hello');
+
+// console.log('Hello');
+
+// alert("Hello");
+// window.alert("Hello1");
+
+// debugger
+console.log('Hello1');
+console.log('Hello2');
+console.log('Hello3');
+console.log('Hello4');
+consoale.log('Hello5');
+console.log('Hello6');
+console.log('Hello7');
+console.log('Hello8');
+console.log('Hello9');
+console.log('Hello10');
---
+++ JSLessons/lesson2-dataTypes/js/script.js
@@ -0,0 +1,63 @@
+// var a = 5;
+// console.log(a);
+// Data Types
+// Primitive DT ->Group
+
+// String
+// var infoString = "5+5";
+// console.log(infoString);
+
+// // Number
+// var infoNumber  = 5+5;
+// console.log(infoNumber);
+
+// // Boolean
+// var infoBoolean = true;
+// console.log(infoBoolean);
+
+// if (false) {
+//     console.log("Hello everyone");
+// }else{
+//     console.log("Bye everyone");
+// }
+
+// var b = "Hello";
+// var a = b ;
+// console.log(a);
+
+// Composite DT ->Group
+// Array
+var a = ["Hello", 5, true];
+console.log(...a);
+
+// Object
+var b = {
+  name: "Elmin",
+  age: 25,
+};
+// console.log(b);
+
+// Special DT ->Group
+// Undefined
+var c = undefined;
+console.log(c);
+
+// Null
+var q = null;
+console.log(q);
+
+
+
+
+// primitive(sadə)
+    // string(mətn)
+    // number(rəqəm)
+    // boolean(true,false)
+
+// composite(birləşik)
+    // array(qovluq)
+    // object(obyekt)
+
+// special(xüsusi)
+    // undefined(təyin olunmamış)
+    // null(boş)
