eff42da174ede8b4fd58e1ddda415a959f67309f
HAsan Bakhtiar <hasan.doctype@gmail.com>
2022-05-19 15:38:17 +0400

    operators

---
+++ JSLessons/lesson3-operators/js/script.js
@@ -0,0 +1,119 @@
+// Operators
+// Assingment opertors
+// =
+// +=
+// -=
+// *=
+// /=
+// %=
+// **=
+
+// Arithmetic Operators 
+// +
+// -
+// *
+// /
+// %
+// **
+// ++
+// --
+
+// Comparison Operator
+// ==
+// ===
+// !=
+// !==
+// <
+// <=
+// >
+// >=
+
+// Logical Operators
+// && - AND (false)
+// || - OR  (true)
+// ! - NOT
+
+// var username = "elmin";
+// var pass = "hasan123";
+
+// if (username == 'hasan' && pass == 'hasan123') {
+//         console.log('login success');
+// }else{
+//     console.log('username or password wrong.');
+// }
+
+
+
+// String Operators
+// var a = "Java"+"Script";
+// // var b = "Script"
+
+// // a+=b;
+// console.log(a);
+
+
+// Special Operators
+// --------------------------------------------------------------------------------------------------
+// Other (Special) Operators (Xüsusi operatorlar) Start
+
+// ,            (vergül)(kodları bir-birindən ayırmaq və sintaksis qanun uyğunluqları icra etmək üçündür )
+// var a = 5, b= 10;
+// console.log(a,b);
+// \            (tərs slash)(Qaçındırma operatorudur)
+// var a = "Hasan\"s Book";
+// console.log(a);
+// typeof       (tip üçün)(datanın tipini təyin etmək üçündür)
+// var a = {};
+// console.log(typeof(a));
+
+// ?:           (sual üst-üstə qoşa nöqtə)((şərt) ? ifadə1 : ifadə2)
+// (5 >= 10) ? document.write("Hi") : document.write("Bye");
+// şərt            ? true                 : false
+
+// 5>10 ? console.log("This is duz") : console.log("this is sehv");
+
+// void         (dəyişkənin dəyərini undefined etmək üçündür)
+// var a = 5;
+
+// console.log(void(a));
+// delete       (ojectin dəyərini silmək üçündür)
+// var a = {
+//     item:"Hello",
+//     item1:"How"
+// }
+// delete a.item;
+// console.log(a.item);
+
+// new          (yeni dəyər yaratməq üçündür)
+// this         (mövcud dəyəri işarələmək üçündür)
+
+// class Human{
+//     constructor(myName){
+//         this.name = myName;
+//     }
+//     sayName(){
+//         return this.name;
+//     }
+// }
+
+// var myHuman = new Human("Hasan");
+// console.log(myHuman.sayName());
+
+
+
+// in           (dəyişkənin daxilində dəyərin olub olmamasını yoxlamaq üçündür)
+// instanceof   (dəyişkənin içində xüsusiyyət olub olmamasını yoxlamaq üçündür)
+
+
+
+
+
+    
+
+// Other (Special) Operators (Xüsusi operatorlar) End
+// --------------------------------------------------------------------------------------------------
+
+
+
+
+
