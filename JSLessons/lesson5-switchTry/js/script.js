068877939cb7e4623a79391668a8bdaafc15d914
Hasan Bakhtiar <hasan.doctype@gmail.com>
2022-05-26 15:59:07 +0400

    loops

---
+++ JSLessons/lesson5-switchTry/js/script.js
@@ -0,0 +1,58 @@
+// const infoText = "a";
+// const infoNumb = 111;
+
+// if (infoText === "a") {
+//     console.log("siz a1 bloga daxil oldunuz");
+//     const infoStr = true;
+//     if (infoNumb < 10) {
+//     console.log("siz a1/1 bloga daxil oldunuz");
+//   } else {
+//     console.log("siz a1/1 bloga daxil ola bilmediniz");
+//     if (infoStr === false) {
+//       console.log("Salam xahis edirik duzgun qurulus yazasiz");
+//     }
+//   }
+// } else if (infoText === "b") {
+//   console.log("siz b1 bloguna daxil oldunuz");
+//   if (infoNumb > 15) {
+//     console.log("siz b1/1 bloguna  daxil oldunuz");
+//   } else {
+//     console.log("siz b1/1 bloga daxil ola bilmediniz");
+//   }
+// }
+
+// const info = 155;
+// switch (info) {
+//   case 5:
+//     console.log("This number is 5");
+//     if (info > 10) {
+//       console.log("this number is little");
+//     } else {
+//       console.log("wrong number");
+//     }
+//     break;
+//   case 15:
+//     console.log("this number is 15");
+//     break;
+//   case "Hello":
+//     console.log("salam");
+//     break;
+//   default:
+//     console.log("this type is default");
+//     if (info === 155) {
+//         console.log('this number is 155');
+//     }
+//     break;
+// }
+
+// try {
+//     console.log("Salam");
+//     throw "her sey qaydasindadir"
+//     console.log("Hi code");
+// } catch (err) {
+//     console.log(err);
+//     // console.log("Texniki xeta var");
+    
+// }finally{
+//     console.log("Men daima ayagdayam");
+// }
---
+++ JSLessons/lesson6-Loops/js/script.js
@@ -0,0 +1,76 @@
+// LOOPS
+// let info = 1000;
+
+// while(info <= 10){ //condition
+//         console.log(info); //result
+//         info++; //action
+
+// }
+
+
+// let info = 0;
+// do{
+//     console.log(info);//result
+//     info++;//action
+// }
+// while(info <= 10); //condition
+
+
+// for(let info = 0; info <= 100; info++){
+//         console.log("JS"+info);
+// }
+
+// const myArr  = [];
+// const myArr  = new Array();
+
+// const infoArr = ["Table","pen","board",5,10,true];
+
+// arraylerin baslangic index deyeri 0-dir.
+// const infoArr = ["table","pen","board"];
+// infoArr[3]= "book";
+// console.log(infoArr[3]);
+
+
+// const infoArr = [];
+// infoArr[0] = "table";
+// infoArr[1] = "pen";
+// infoArr[2] = "book";
+// console.log(infoArr);
+
+// const infoArr = ["Table","pen","board",5,10,true];
+
+// infoArr[1] = "copybook";
+// console.log(infoArr);
+
+// const infoArr = ["Table","pen","board",5,10,true];
+// infoArr['myname'] = "board";
+// console.log(infoArr['myname']);
+
+// const myArr = ['table','book',[true,[10,15,200,[false,['new']],'summer']],'copybook'];
+// console.log(myArr[2].length);
+// console.log(myArr[2][1][3][1][0]);
+
+
+
+// const infoArr = ["table","pen","board"];
+
+// for(let a = 0; a<infoArr.length; a++){
+//     console.log(infoArr[a]);
+// }
+
+
+// const infoArr = ["table","pen","board"];
+
+// for( x in infoArr){
+//     console.log(infoArr[x]);
+// }
+// const cars = ["BMW", "Volvo", "Mini"];
+
+// let text = "";
+// for (let x of cars) {
+//   text += x + "<br>";
+// }
+
+// document.getElementById("demo").innerHTML = text;
+
+
