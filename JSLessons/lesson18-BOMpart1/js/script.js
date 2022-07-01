15773f1a0433157441723cb312585caf6b8e1778
Hasan Bakhtiar <hasan.doctype@gmail.com>
2022-07-01 15:01:42 +0400

    BOM end

---
+++ JSLessons/lesson18-BOMpart1/js/script.js
@@ -0,0 +1,45 @@
+// const listData = document.querySelector('span');
+
+// listData.innerHTML = navigator.appCodeName;
+// listData.innerHTML = navigator.appName;
+// listData.innerHTML = navigator.product;
+// listData.innerHTML = navigator.appVersion;
+// listData.innerHTML = navigator.userAgent;
+// listData.innerHTML = navigator.platform;
+// listData.innerHTML = navigator.language;
+// listData.innerHTML = navigator.onLine;
+
+
+// ALert Prompt
+
+// const alertData = window.prompt("Melumat Daxil edin","Hello Man");
+// listData.innerHTML = alertData;
+
+
+
+// // Timing
+// const dataArray = [];
+// const writingData = ()=>{
+// //    document.querySelector('span').innerHTML = 'Hello <br>';
+// // console.log("Hello");
+// dataArray.push("Hello");
+// // console.log(dataArray);
+
+// document.querySelector('p').innerHTML = dataArray;
+// }
+// // 1000ms = 1s
+// const intervalData =  setInterval(writingData,1000);
+
+// document.querySelector('button').onclick = ()=>{clearInterval(intervalData)}
+
+
+// // Timing
+// const writingData = ()=>{
+//     document.write('Hello <br>');
+// }
+// // 1000ms = 1s
+// setTimeout(writingData,5000);
+
+
+// const a =()=>{ document.querySelector('span').innerHTML = new Date().getMilliseconds();}
+// setInterval(a,1)
