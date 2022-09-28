897c00de38d24abaa4201958bb3a3b9442d1ef7d
Hasan Bakhtiar <hasan.doctype@gmail.com>
2022-09-28 13:15:16 +0400

    axios fetch

--- JSLessons/TypeScript/lesson4-typescriptreact/src/index.tsx
+++ JSLessons/TypeScript/lesson4-typescriptreact/src/index.tsx
@@ -1,17 +1,26 @@
-import React, { useState } from 'react';
+import React,{useEffect, useState} from 'react';
 import ReactDOM from 'react-dom/client';
+import axios from 'axios';
+
 const DataList = (props:any) =>{
   return(
-    <li>{props.fd}</li>
+    <li>{props.name}</li>
   )
 }
 const App =()=>{
-  const [data,setData] = useState("New");
+  const [data,setData] = useState();
+  useEffect(()=>{
+  axios.get('https://restcountries.com/v3.1/all')
+  .then((res:any)=>setData(res.data[0].name.common))
+  // .then((res:any)=>console.log(res.data[0].name.common))
+  .catch(err=>console.log(err))
 
+},[])
   return(
-    <h1>
-      <DataList fd={data}/> 
-    </h1>
+  <ul>
+   
+    <DataList name={data}/>
+  </ul>
   )
 }
 const root = ReactDOM.createRoot(
